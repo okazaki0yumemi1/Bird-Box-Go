93af4bd0429755e48e95134fbb79edc55ff8fba3
Dmitry Melnikov <suwako.moria@gmail.com>
2025-01-09 20:55:39 +0300

    Create migrations table

---
+++ cmd/migrations/birds_table.sql
@@ -0,0 +1,17 @@
+-- +goose Up
+-- SQL in this section is executed when the migration is applied.
+CREATE TABLE IF NOT EXISTS birds
+(
+    id                  UUID        PRIMARY KEY,
+    name                TEXT        NOT NULL,
+    confidence          TEXT        NOT NULL,
+    recoding_date       DATE        NULL,
+    microphone_name     TEXT        NULL,
+    created_at          TIMESTAMP   NOT NULL,
+    updated_at          TIMESTAMP   NOT NULL,
+    deleted_at          TIMESTAMP   NULL
+);
+
+-- +goose Down
+-- SQL in this section is executed when the migration is rolled back.
+DROP TABLE IF EXISTS books;
