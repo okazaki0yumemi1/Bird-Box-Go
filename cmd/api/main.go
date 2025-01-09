2e44938be443851e928bae7e7d23f7392ffeed72
Dmitry Melnikov <suwako.moria@gmail.com>
2025-01-09 21:44:48 +0300

    Apply env file params

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,19 +5,34 @@ import (
 	"log"
 	"net/http"
 
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	gormlogger "gorm.io/gorm/logger"
+
 	"bird-box-go/api/router"
 	"bird-box-go/config"
 )
 
-//  @title          Bird-Box-Go API
-//  @version        1.0
-//  @description    This is a work-in-progress version of a RESTful API with a CRUD functionality to control & access an app. Most things are taken from https://learning-cloud-native-go.github.io
-//  @host       localhost:8080
-//  @basePath   /v1
+const fmtDBString = "host=%s user=%s password=%s dbname=%s port=%d sslmode=disable"
 
 func main() {
 	c := config.New()
-	r := router.New()
+
+	var logLevel gormlogger.LogLevel
+	if c.DB.Debug {
+		logLevel = gormlogger.Info
+	} else {
+		logLevel = gormlogger.Error
+	}
+
+	dbString := fmt.Sprintf(fmtDBString, c.DB.Host, c.DB.Username, c.DB.Password, c.DB.DBName, c.DB.Port)
+	db, err := gorm.Open(postgres.Open(dbString), &gorm.Config{Logger: gormlogger.Default.LogMode(logLevel)})
+	if err != nil {
+		log.Fatal("DB connection start failure")
+		return
+	}
+
+	r := router.New(db)
 	s := &http.Server{
 		Addr:         fmt.Sprintf(":%d", c.Server.Port),
 		Handler:      r,
