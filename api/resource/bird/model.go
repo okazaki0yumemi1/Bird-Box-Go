052fb693b3cecd41d0a8f0ceb646f23fa3af640c
Dmitry Melnikov <suwako.moria@gmail.com>
2025-01-09 21:19:16 +0300

    Moved to ORM

--- api/resource/bird/model.go
+++ api/resource/bird/model.go
@@ -1,18 +1,34 @@
 package bird
 
-type DTO struct {
-	ID            string `json:"id"`
-	CommonName    string `json:"name"`
-	Confidence    string `json:"confidence"`
-	RecordingDate string `json:"recoding_date"`
-	InputDevice   string `json:"microphone_name"`
-	// Description   string `json:"description"`
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+// type DTO struct {
+// 	ID            string `json:"id"`
+// 	CommonName    string `json:"name"`
+// 	Confidence    string `json:"confidence"`
+// 	RecordingDate string `json:"recoding_date"`
+// 	InputDevice   string `json:"microphone_name"`
+// 	// Description   string `json:"description"`
+// }
+
+// type Form struct {
+// 	CommonName    string `json:"name"`
+// 	Confidence    string `json:"confidence"`
+// 	RecordingDate string `json:"recoding_date"`
+// 	InputDevice   string `json:"microphone_name"`
+// 	// Description   string `json:"description"`
+// }
+
+type Bird struct {
+	ID            uuid.UUID `gorm:"primarykey"`
+	CommonName    string
+	Confidence    string
+	RecordingDate time.Time
+	InputDevice   string
 }
 
-type Form struct {
-	CommonName    string `json:"name"`
-	Confidence    string `json:"confidence"`
-	RecordingDate string `json:"recoding_date"`
-	InputDevice   string `json:"microphone_name"`
-	// Description   string `json:"description"`
-}
+type Birds []*Bird
